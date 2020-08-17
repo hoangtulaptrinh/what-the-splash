491c506f07b3d2f1e9720348ed70b6dc75845559
Đỗ Tùng Dương <duong98kk@gmail.com>
2020-08-17 14:57:14 +0700

    Update example.js

--- src/sagas/example.js
+++ src/sagas/example.js
@@ -33,6 +33,8 @@ export default rootSaga;
 // Put(): Dùng để dispatch một action (non-block)
 // delay(): Dùng để chặn (block) trong 1 khoảng thời gian VD: yield delay(1000) tức là đợi thêm 1s rồi chạy code ở dưới
 // takeEvery(): Theo dõi một action nào đó thay đổi thì gọi một saga nào đó
-// takeLastest() : Có nghĩa là nếu chúng ta thực hiện một loạt các actions, nó sẽ chỉ thực thi và trả lại kết quả của của actions cuối cùng
+// takeLastest() : Có nghĩa là nếu chúng ta thực hiện một loạt các actions, nó sẽ chỉ thực thi và trả lại kết quả của của actions cuối cùng,
+//                 tức là nó sẽ hủy bỏ quy trình cũ khi có quy trình mới bắt đầu VD : bấm liên tục vào 1 nút để gọi api thì sẽ chỉ nhận lần bấm cuối
+//                 nó sẽ không gọi liên tục nếu như lần bấm trước chưa g
 // yield(): Có nghĩa là chạy tuần tự khi nào trả ra kết quả mới thực thi tiếp
 // Select(): Chạy một selector function để lấy data từ state
